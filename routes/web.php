f3fbb9dc39322b34cdfb6ee21d119c86f869a6dd
welitonlimaa <welitonlima159@gmail.com>
2023-06-05 10:42:47 -0300

    feat: dynamic navlink by role

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'role:user'])->name('user.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
