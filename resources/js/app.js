93ac0c25fb8a12a97a3d60abde3fecd8710737e1
welitonlimaa <welitonlima159@gmail.com>
2023-06-05 21:59:38 -0300

    feat: ham menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,19 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const ham = document.getElementById('ham-svg');
+const x = document.getElementById('x-svg');
+const navLinks = document.getElementById('nav-links');
+
+ham.addEventListener('click', () => {
+  ham.classList.add("hidden");
+  x.classList.remove("hidden");
+  navLinks.classList.remove("hidden");
+});
+
+x.addEventListener('click', () => {
+  ham.classList.remove("hidden");
+  x.classList.add("hidden");
+  navLinks.classList.add("hidden");
+});
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased min-w-full min-h-screen text-gray-900 bg-center bg-gray-100 selection:bg-red-500 selection:text-white">
+        @include('components.header')
+    </body>
+</html>
