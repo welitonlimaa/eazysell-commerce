9584d75299eb4414db79035cb253d70b8a428d2d
welitonlimaa <welitonlima159@gmail.com>
2023-06-15 20:28:47 -0300

    feat: add logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,8 +2,7 @@
     <nav class="p-4">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="{{ route('home') }}" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap">Flowbite</span>
+                <img src="https://github.com/welitonlimaa/eazysell-commerce/blob/main/resources/images/logo.png" alt="Eazysell Logo" class="mr-3 h-6 sm:h-9" />
             </a>
             <div class="flex items-center lg:order-2">
                         <a href="{{ route('cart') }}" class="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">
