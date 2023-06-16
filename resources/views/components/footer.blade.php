e97a56da6ef01e789fece5e92fe22f0a55747da6
welitonlimaa <welitonlima159@gmail.com>
2023-06-16 11:19:54 -0300

    style: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
 <!-- component -->
-<footer class="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100">
+<footer class="bg-gradient-to-r from-orange-300 via-[#eb720f] to-orange-300">
   <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
     <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
       <div>
-      <img src="#" class="mr-5 h-6 sm:h-9" alt="logo" />
+      <img src="https://github.com/welitonlimaa/eazysell-commerce/assets/108986668/dd5db357-f416-4fa5-b68a-a17f8de2d92f" class="mr-5 h-6 sm:h-9" alt="logo" />
         <p class="max-w-xs mt-4 text-sm text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, accusantium.
         </p>
@@ -42,10 +42,10 @@
       </div>
       <div class="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
         <div>
-          <p class="font-medium">
+          <p class="font-bold">
             Company
           </p>
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+          <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
             <a class="hover:opacity-75" href> About </a>
             <a class="hover:opacity-75" href> Meet the Team </a>
             <a class="hover:opacity-75" href> History </a>
@@ -53,32 +53,20 @@
           </nav>
         </div>
         <div>
-          <p class="font-medium">
-            Services
-          </p>
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-            <a class="hover:opacity-75" href> 1on1 Coaching </a>
-            <a class="hover:opacity-75" href> Company Review </a>
-            <a class="hover:opacity-75" href> Accounts Review </a>
-            <a class="hover:opacity-75" href> HR Consulting </a>
-            <a class="hover:opacity-75" href> SEO Optimisation </a>
-          </nav>
-        </div>
-        <div>
-          <p class="font-medium">
+          <p class="font-bold">
             Helpful Links
           </p>
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+          <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
             <a class="hover:opacity-75" href> Contact </a>
             <a class="hover:opacity-75" href> FAQs </a>
             <a class="hover:opacity-75" href> Live Chat </a>
           </nav>
         </div>
         <div>
-          <p class="font-medium">
+          <p class="font-bold">
             Legal
           </p>
-          <nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+          <nav class="flex flex-col mt-4 space-y-2 text-sm text-white">
             <a class="hover:opacity-75" href> Privacy Policy </a>
             <a class="hover:opacity-75" href> Terms &amp; Conditions </a>
             <a class="hover:opacity-75" href> Returns Policy </a>
@@ -88,7 +76,7 @@
       </div>
     </div>
     <p class="mt-8 text-xs text-gray-800">
-      © 2022 Comany Name
+      © 2023
     </p>
   </div>
 </footer>
