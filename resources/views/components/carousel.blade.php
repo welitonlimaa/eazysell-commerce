19d1daf098d859d295f15701590b921633b2e5af
welitonlimaa <welitonlima159@gmail.com>
2023-06-16 11:07:12 -0300

    style: add cart icon change logo

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,23 +1,13 @@
-<!-- This is an example component -->
 <div class="min-w-full mx-auto">
 	<div id="default-carousel" class="relative" data-carousel="static">
-        <!-- Carousel wrapper -->
         <div class="overflow-hidden relative h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+                <img src="https://github.com/welitonlimaa/eazysell-commerce/assets/108986668/4dd6bdf6-b191-4e23-a5b0-ed4f060a27b6" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
             </div>
-            <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
+                <img src="https://github.com/welitonlimaa/eazysell-commerce/assets/108986668/31a1e61f-8cd7-4016-9696-311b8a5a853a" class="block absolute object-none object-top top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="...">
             </div>
         </div>
-        <!-- Slider indicators -->
         <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
             <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
